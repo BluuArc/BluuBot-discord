8b4511fbf9319ca125895b536d824f78f54e6581
Joshua Castor <jcasto3@uic.edu>
2017-03-25 23:59:51 -0500

    Add bot files

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var assert = require('assert');
+var bluubot = require('../lib');
+
+describe('bluubot', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
